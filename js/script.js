25ff1f2488fbb2b6d406d5bfbd416e3ae6031536
hossain <hossainmahmud.cse@gmail.com>
2023-10-19 21:41:24 +0600

    addde

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// ========== slider part start
+
+$('.cards_row').slick({
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay:true,
+    prevArrow:'<i class="fa-solid fa-chevron-left prev_arrow"></i>',
+    nextArrow:'<i class="fa-solid fa-chevron-right next_arrow"></i>',
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  // ========== slider part end 
