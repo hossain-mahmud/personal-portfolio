ca948b779581ede9c32c8d469d13d7f143c4fcb9
hossain <hossainmahmud.cse@gmail.com>
2023-10-28 19:39:03 +0600

    medium devices responsive added

--- js/script.js
+++ js/script.js
@@ -10,12 +10,19 @@ $('.cards_row').slick({
   nextArrow: '<i class="fa-solid fa-chevron-right next_arrow"></i>',
   dots: true,
   responsive: [{
-      breakpoint: 1024,
+      breakpoint: 1200,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
         infinite: true,
         dots: true
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
       }
     },
     {
